531147706d1898abc576d51b51ae52191c230c4a
Leo <leo91yang@gmail.com>
2023-07-02 22:31:53 +0800

    Object detection with cell phone and tablet

--- app/src/main/java/com/example/learningassistance/facedetection/HeadPoseAnalysis.kt
+++
@@ -1,4 +0,0 @@
-package com.example.learningassistance.facedetection
-
-class HeadPoseAnalysis {
-}
---
+++ app/src/main/java/com/example/learningassistance/graphicOverlay/ObjectDetectionGraphicOverlay.kt
@@ -0,0 +1,4 @@
+package com.example.learningassistance.graphicOverlay
+
+class ObjectDetectionOverlay {
+}
